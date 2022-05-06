095f595a1edc81a7c8450c063cc0498e5879a3ca
jiwon-developer <oh.jiiwon@gmail.com>
2022-05-06 16:52:24 -0700

    Apply mongoose (auth)

--- data/auth.js
+++ data/auth.js
@@ -1,32 +1,50 @@
 //MODEL
 
-import { getUsers } from "../database/database.js";
-import MongoDb from "mongodb";
+import Mongoose from "mongoose";
+import { useVirtualId } from "../database/database.js";
 
-const ObjectId = MongoDb.ObjectId;
+//const ObjectId = MongoDb.ObjectId;
+
+//schema
+const userSchema = new Mongoose.Schema({
+  username: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  url: String,
+});
+
+useVirtualId(userSchema);
+//Create model and link  User collection to userSchema
+const User = Mongoose.model("User", userSchema);
 
 export async function findByUsername(username) {
-  return getUsers()
-    .findOne({ username }) //
-    .then(mapOptionalUser);
+  // return getUsers()
+  //   .findOne({ username })
+  //   .next()
+  //   .then(mapOptionalUser);
+  return User.findOne({ username });
 }
 
 export async function findById(id) {
-  return getUsers()
-    .findOne({ _id: new ObjectId(id) })
-    .then((data) => {
-      return mapOptionalUser(data);
-    });
+  return User.findById(id);
+  // return getUsers()
+  //   .findOne({ _id: new ObjectId(id) })
+  //   .next()
+  //   .then((data) => {
+  //     return mapOptionalUser(data);
+  //   });
 }
 
 export async function createUser(user) {
-  return getUsers()
-    .insertOne(user)
-    .then((data) => data.insertedId.toString());
+  return new User(user).save().then((data) => data.id);
+  // return getUsers()
+  //   .insertOne(user)
+  //   .then((data) => data.insertedId.toString());
 }
 
 // return user's new object if user exist
 // if not return null
-function mapOptionalUser(user) {
-  return user ? { ...user, id: user._id.toString() } : null;
-}
+// function mapOptionalUser(user) {
+//   return user ? { ...user, id: user._id.toString() } : null;
+// }
--- database/database.js
+++ database/database.js
@@ -1,15 +1,24 @@
-import { MongoClient } from "mongodb";
+import Mongoose from "mongoose";
 import { config } from "../config.js";
 
-let db;
 //promise return
 export async function connectDB() {
-  return new MongoClient(config.db.host)
-    .connect() //
-    .then((client) => {
-      db = client.db();
-    });
+  return Mongoose.connect(config.db.host);
 }
+
+export function useVirtualId(schema) {
+  //_id -> id :  ObjectId(id).toString()
+  schema.virtual("id").get(function () {
+    return this._id.toString();
+  });
+
+  // json으로 변환시 가상의 요소도 포함시키기
+  schema.set("toJSON", { virtuals: true });
+  schema.set("toObject", { virtuals: true });
+}
+
+//TODO (Jiwon) : Delete below
+let db;
 
 export function getUsers() {
   return db.collection("users");
